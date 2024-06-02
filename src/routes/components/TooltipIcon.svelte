dfd1fa0f631d5f2fa060a1ea8b4d63d0ebffb465
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2024-06-02 17:03:42 -0400

    feat: add fly-in animation

--- src/routes/components/TooltipIcon.svelte
+++ src/routes/components/TooltipIcon.svelte
@@ -45,7 +45,7 @@
       style:left="{left}px"
       style:top="{top}px"
       style:max-width={maxWidth}
-      transition:fly={{ x: 0, duration: 300 }}
+      transition:fly={{ x: -8, duration: 300 }}
       on:mouseenter={onHover}
       on:mouseleave={offHover}
       class={`absolute z-10 whitespace-pre-line rounded border-4 border-border bg-background p-4 text-text shadow-hard-md transition-opacity duration-md ease-in-out`}
